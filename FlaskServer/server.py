6afbfb1f0970c21e77d52c1c427812df7c412dd5
ewoodbury62 <69811900+ewoodbury62@users.noreply.github.com>
2023-03-30 12:28:57 -0500

    Update server.py

--- FlaskServer/server.py
+++ FlaskServer/server.py
@@ -2,17 +2,21 @@
 from flask import Flask, render_template, request
 from picamera import PiCamera
 import time
-app = Flask(__name__)
+import os
 
+PEOPLE_FOLDER = os.path.join('static', 'people_photo')
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = PEOPLE__FODLER
 @app.route('/', methods=['GET', 'POST'])
 def index():
+	full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'img.jpg')
 	if request.method == 'POST':
 		if request.form.get('camera') == 'CAMERA':
 			takePic()
 	elif request.method == 'GET':
 		return render_template('index.html')
 
-	return render_template('index.html')
+	return render_template('index.html', user_image = full_filename)
 
 
 def takePic():
